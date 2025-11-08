9f392be0de5d9a48ec167b4bad6a88883dc16fcd
byrybdyk <90798897+byrybdyk@users.noreply.github.com>
2025-11-08 13:11:09 +0300

    fix cors v3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import fs from 'fs'
 import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, '../tls/frontend/frontend.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, '../tls/frontend/frontend-fullchain.crt')),
+export default defineConfig(({ command }) => {
+  // command === 'serve' → dev; command === 'build' → прод-сборка
+  const isDev = command === 'serve'
+
+  return {
+    plugins: [vue(), vueDevTools()],
+    server: isDev
+      ? {
+          https: {
+            key: fs.readFileSync(path.resolve(__dirname, '../tls/frontend/frontend.key')),
+            cert: fs.readFileSync(
+              path.resolve(__dirname, '../tls/frontend/frontend-fullchain.crt'),
+            ),
+          },
+          host: '0.0.0.0',
+          port: 5173,
+          strictPort: true,
+        }
+      : undefined,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+  }
 })
